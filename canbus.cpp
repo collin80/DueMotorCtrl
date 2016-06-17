99331ca5b4bb0b4d1514be97629b513c85156396
Collin Kidder <collink@kkmfg.com>
2016-06-17 14:11:59 -0400

    Re-enable canbus (was accidentally committed disabled)

--- canbus.cpp
+++ canbus.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
-//#include <due_can.h>
+#include <due_can.h>
 #include "canbus.h"
 #include "config.h"
 
 void setup_CAN()
 {
-    //Can0.begin(settings.canSpeed);
-    //Can0.setRXFilter(0, 0x7F0, settings.canBaseRx, false);
+    Can0.begin(settings.canSpeed);
+    Can0.setRXFilter(0, 0x7F0, settings.canBaseRx, false);
 }
 
 void canRX()
